d6237dbdd81bd56e01843d9e33183d858f54448f
ZoeleneVelinsky <24374107@student.uwa.edu.au>
2026-05-13 22:01:35 +0800

    Update the model,py and make sure logo's colour is black on light mode

--- app/models.py
+++ app/models.py
@@ -43,6 +43,7 @@ class User(db.Model, UserMixin):
     pfp_filepath = db.Column(db.String(256), nullable=True)
     user_tier = db.Column(db.String(10), nullable=False, default='free')
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    darkmode = db.Column(db.Boolean, nullable=False, default=True)
 
     # relationships
     notes = db.relationship('Note', back_populates='user', foreign_keys='Note.user_id')
